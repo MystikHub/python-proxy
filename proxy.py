035e2c9caed7848058a70f838ac09434865d8fd1
MystikHub <shutanu@pm.me>
2021-03-13 21:52:29 +0000

    Proxy works now

--- proxy.py
+++ proxy.py
@@ -1,31 +1,42 @@
 import socket
+import time
+from threading import Thread
 
 HOST = ''
 PORT = 4000
-BUFF_SIZE = 4096
+BUFF_SIZE = 65536
+waitingToStart = []
 
-def getRequestText(conn, sendback=False):
-    print('Entering getRequestText()')
+def getRequestText(conn):
+    start = time.time()
+    # print('Entering getRequestText()')
     requestText = ""
-    while True:
-        data = conn.recv(BUFF_SIZE)
-        if not data: break
-        if sendback:
-            conn.sendall(data)
-        try:
-            requestText += data.decode('ISO-8859-1')
-        except Exception as e:
-            print(e)
-        print("Got some data:\n\n{}".format(data.decode('ISO-8859-1')))
 
-    print('Got a (hopefully) http request:\n{}'.format(requestText))
+    stillreceiving = True
+    while stillreceiving:
+        try:
+            data = conn.recv(BUFF_SIZE)
+            if data == b'':
+                stillreceiving = False
+            else:
+                requestText += data.decode('ISO-8859-1')
+            if data.decode('ISO-8859-1').endswith("\r\n\r\n"):
+                stillreceiving = False
+        except BlockingIOError:
+            pass
+
+    # print('Got a (hopefully) http request:\n{}'.format(requestText))
+    diff = time.time() - start
+    if diff > 0.6: print("Request collection took " + diff + " seconds")
+
     return requestText
 
 def getRequestType(requestText):
-    print('Entering getRequestType()')
+    start = time.time()
+    # print('Entering getRequestType()')
     if requestText == '':
-        print('requestText was empty')
-        return 'other', 'localhost'
+        # print('requestText was empty')
+        return None
 
     # If it's an HTTP request
     requestLines = requestText.split('\n')
@@ -33,61 +44,88 @@ def getRequestType(requestText):
 
     HttpType = firstLineSplit[0]
 
-    requestHost = ""
-    requestPort = 0
+    requestHost = None
+    requestPort = -1
     for line in requestLines:
         if line.startswith("Host: "):
             url = line.split()[1].split(':')
             requestHost = url[0]
-            if requestHost.startswith("www."):
-                requestHost = requestHost[4:]
             requestPort = int(url[1]) if len(url) > 1 else 80
 
-    print('Got a {} request for {}'.format(HttpType, requestHost))
+    # print('Got a {} request for {}'.format(HttpType, requestHost))
+    diff = time.time() - start
+    if diff > 0.6: print("Request parsing took " + diff + " seconds")
+
     return HttpType, requestHost, requestPort
 
-def forwardRequest(client):
-    print('Entered forwardRequest()')
-    requestText = getRequestText(client)#, sendback=True)
+def forwardConnection(client):
+    start = time.time()
+    global waitingToStart
+    # print("Connections waiting to start: {}".format(waitingToStart))
+    # print('Entered forwardRequest()')
+    requestText = getRequestText(client)
     HttpType, requestHost, requestPort = getRequestType(requestText)
-    print(HttpType + ', ' + requestHost)
+    waitingToStartMember = HttpType + ', ' + requestHost + ', ' + str(requestPort)
+    print(waitingToStartMember)
+    waitingToStart.append(waitingToStartMember)
 
     # print(requestText)
     serversock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serversock.settimeout(5)
 
     if HttpType == 'CONNECT':
-        print('We\'ve got a CONNECT request')
-        client.sendall("HTTP/1.1 200 OK \r\n".encode())
+        # print('We\'ve got a CONNECT request')
+        client.sendall("HTTP/1.1 200 OK \r\n\r\n".encode('ISO-8859-1'))
 
     serversock.connect((requestHost, requestPort))
-        
-    print('Connecting to {}:{}'.format(requestHost, requestPort))
-    serversock.sendall(requestText.encode())
+    serversock.setblocking(0)
 
-    print("Starting response receival loop")
-    response = ''
+    # print('Connecting to {}:{}'.format(requestHost, requestPort))
+
+    waitingToStart.remove(waitingToStartMember)
+    # print("Connection completed, remaining: {}".format(len(waitingToStart)))
+    # repetitions = 0
+    diff = time.time() - start
+    if diff > 0.6: print("Connection took " + diff + " seconds")
     while True:
-        data = serversock.recv(BUFF_SIZE)
-        if not data:
-            print("{}, {}, {}".format(requestHost, requestPort, serversock))
-            print('Response has ended')
+        somethingDone = False
+        try:
+            data = client.recv(BUFF_SIZE)
+            serversock.sendall(data)
+            # if len(data.decode('ISO-8859-1')) > 1:
+            #     somethingDone = True
+        except BlockingIOError:
+            pass
+        except ConnectionResetError:
             break
-        else:
-            try:
-                response += data.decode('ISO-8859-1')
-            except e:
-                print(e)
-            client.sendall(data)
+        except BrokenPipeError:
+            break
 
-    print('Response: ' + response)
+        try:
+            data = serversock.recv(BUFF_SIZE)
+            client.sendall(data)
+            # if len(data.decode('ISO-8859-1')) > 1:
+            #     somethingDone = True
+        except BlockingIOError:
+            pass
+        except ConnectionResetError:
+            break
+        except BrokenPipeError:
+            break
+
+        # if somethingDone:
+        #     repetitions += 1
+        #     print("Connection repetitions: {}".format(repetitions))
+
     serversock.close()
-    conn.close()
+    client.close()
 
 if __name__ == "__main__":
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind((HOST, PORT))
         s.listen()
-        conn, addr = s.accept()
-        with conn:
-            forwardRequest(conn)
+        while True:
+            conn, addr = s.accept()
+            conn.setblocking(0)
+            request = Thread(target=forwardConnection, args=(conn, ))
+            request.start()
